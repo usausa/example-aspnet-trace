6abf4e779f8ef2651ce5c7c0d09c3600f9fe6382
usausa <machi.pon@gmail.com>
2026-09-02 14:32:13 +0900

    Update

--- Example.SourceGenerator/Assembly.cs
+++ Example.SourceGenerator/Assembly.cs
@@ -7,9 +7,7 @@
 namespace System.Runtime.CompilerServices
 {
     // For compatibility
-    public sealed class IsExternalInit
-    {
-    }
+    public sealed class IsExternalInit;
 }
 #pragma warning restore IDE0161
 #pragma warning restore IDE0130
